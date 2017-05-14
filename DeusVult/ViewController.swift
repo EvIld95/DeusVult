4cd44773ff0d39bb960df34ba4cb08b37dcd2cb3
Paweł Szudrowicz <pablo.szudrowicz@gmail.com>
2017-05-14 13:56:28 +0200

    Init project

---
+++ DeusVult/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeusVult
+//
+//  Created by Paweł Szudrowicz on 14.05.2017.
+//  Copyright © 2017 Paweł Szudrowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
